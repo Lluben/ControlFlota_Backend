d32fff5062a3e52e88a38785ebab53dec6cda9e1
Lluben <chayanperu@gmail.com>
2025-06-11 11:31:46 -0500

    Cors mas eleborado

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ const store = new sessionStore({
 };*/
 
 const corsOptions = {
-    origin: '*',
-    credentials: false,
+    origin: ['http://54.160.158.246','http://localhost:5000'],
+    credentials: true,
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type',
+    optionsSuccessStatus: 200
 };
 
 
